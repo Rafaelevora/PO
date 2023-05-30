04f90464abb1e99b8a2e098906b245c5d411bb3f
Rafael Évora <Rafael.evora.empresarial@gmail.com>
2023-05-30 20:57:49 -0300

    Atualização

--- main.c
+++ main.c
@@ -31,25 +31,15 @@ typedef struct grafo
 Fila *cria_fila();
 Fila *enqueue(Fila *f, void *elemento);
 void *dequeue(Fila *f);
-int eh_vazia_fila(Fila *f);
 Listad *cria_listad();
 void mostra_listad(Listad* L);
 Nod *cria_nod(void *valor);
-Listad *insere_inicio_listad(Listad *L, void *valor);
 Listad *insere_fim_listad(Listad *L, void *valor);
-void *remove_fim_listad(Listad *L);
 void *remove_inicio_listad(Listad *L);
 Listad *libera_listad(Listad *L);
-int eh_vazia_listad(Listad *L);
 Grafo *cria_grafo(int n, int valorado, int direcionado);
-void preenche_vertices(Grafo *g);
 void preenche_arestas(Grafo *g);
-void mostra_grafo(Grafo *g);
-void percurso_largura(Grafo *g);
-void profundidade(Grafo *g);
-int verifica_grafo(Grafo *g);
-void percurso_larg(Grafo *g, int vertice);
-Listad *percurso_larg_lim(Grafo *g, int vertice, int lim, int vertice_inicial);
+void *percurso_larg_lim(Grafo *g, int vertice, int lim, int vertice_inicial);
 void swapList(Nod *a, Nod *b);
 void sort_vertices_byValue(Listad *L);
 int *create_int(int valor);
@@ -85,15 +75,13 @@ int main()
                 scanf("%d", &y);
                 g->adjacencias[x - 1][y - 1] = g->adjacencias[y - 1][x - 1] = 1;
             }
-            printf("Teste %d\n", contador);
-            //for (int i = 0; i < l; i++)
-            //{
+
+                printf("Teste %d\n", contador);
                 percurso_larg_lim(g, l, p, l);
                 sort_vertices_byValue(g->lista);
                 mostra_listad(g->lista);
 
                 printf("\n");
-            //}
             contador++;
         }
 
@@ -116,18 +104,6 @@ void *dequeue(Fila *f)
     return remove_inicio_listad(f);
 }
 
-int eh_vazia_fila(Fila *f)
-{
-    return eh_vazia_listad(f);
-}
-
-int *cria_inteiro(int n)
-{
-    int *a = malloc(sizeof(int));
-    *a = n;
-    return a;
-}
-
 Listad *cria_listad()
 {
     Listad *novalista;
@@ -142,29 +118,6 @@ Nod *cria_nod(void *valor)
     novo->ant = novo->prox = NULL;
     novo->info = valor;
     return novo;
-}
-
-Listad *insere_inicio_listad(Listad *L, void *valor)
-{
-    Nod *novo = cria_nod(valor);
-
-    if (L == NULL)
-    {
-        L = cria_listad();
-        L->ini = L->fim = novo;
-    }
-    else
-    {
-        if (L->ini == NULL)
-            L->ini = L->fim = novo;
-        else
-        {
-            novo->prox = L->ini;
-            L->ini->ant = novo;
-            L->ini = novo;
-        }
-    }
-    return L;
 }
 
 Listad *insere_fim_listad(Listad *L, void *valor)
@@ -216,28 +169,6 @@ void *remove_inicio_listad(Listad *L)
     return resposta;
 }
 
-void *remove_fim_listad(Listad *L)
-{
-    Nod *aux;
-    void *resposta = NULL;
-
-    if (L != NULL && L->fim != NULL)
-    {
-        aux = L->fim;
-        if (L->ini != L->fim) // mais de um elemento na lista
-        {
-            L->fim->ant->prox = NULL;
-            L->fim = L->fim->ant;
-        }
-        else // s� tem um elemento na lista
-            L->ini = L->fim = NULL;
-
-        resposta = aux->info;
-        free(aux);
-    }
-    return resposta;
-}
-
 Listad *libera_listad(Listad *L)
 {
     Nod *aux = L->ini;
@@ -254,13 +185,6 @@ Listad *libera_listad(Listad *L)
     return NULL;
 }
 
-int eh_vazia_listad(Listad *L)
-{
-    if (L == NULL || L->ini == NULL)
-        return 1;
-    else
-        return 0;
-}
 
 Grafo *cria_grafo(int n, int valorado, int direcionado)
 {
@@ -279,13 +203,6 @@ Grafo *cria_grafo(int n, int valorado, int direcionado)
     return g;
 }
 
-void preenche_vertices(Grafo *g)
-{
-    int i;
-    for (i = 0; i < g->ordem; i++)
-        g->vertices[i] = i + 1;
-}
-
 void preenche_arestas(Grafo *g)
 {
     int i, j;
@@ -293,159 +210,6 @@ void preenche_arestas(Grafo *g)
         for (j = 0; j < g->ordem; j++)
             // scanf("%d", &g->adjacencias[i][j]);
             g->adjacencias[i][j] = 0;
-}
-
-void mostra_grafo(Grafo *g)
-{
-    int i, j;
-    for (i = 0; i < g->ordem; i++)
-    {
-        printf("%i -> ", g->vertices[i]);
-        for (j = 0; j < g->ordem; j++)
-            if (g->adjacencias[i][j] != 0)
-                printf("%d - ", j + 1);
-    }
-}
-
-void limpa(Grafo *g)
-{
-    int i;
-    for (i = 0; i < g->ordem; i++)
-        g->visitados[i] = 0;
-}
-
-void percurso_largura(Grafo *g)
-{
-    Fila *f = cria_fila();
-    int *w, *nro;
-    int i;
-
-    limpa(g);
-    g->visitados[0] = 1; // visitei o primeiro vertice
-    w = (int *)malloc(sizeof(int));
-    *w = 0;
-    enqueue(f, (void *)nro);
-
-    while (!eh_vazia_fila(f))
-    {
-        w = (int *)dequeue(f);
-        printf("%d \n", g->vertices[*w]);
-        for (i = 0; i < g->ordem; i++)
-        {
-            if (g->adjacencias[*w][i] == 1)
-            {
-                if (!g->visitados[i])
-                {
-                    g->visitados[i] = 1; // visitei o primeiro vertice
-                    nro = (int *)malloc(sizeof(int));
-                    *nro = i;
-                    enqueue(f, (void *)nro);
-                }
-            }
-        }
-    }
-}
-
-void prof(int v, Grafo *g)
-{
-    int w;
-    g->visitados[v] = 1;
-    printf("%d \n", v + 1);
-    for (w = 0; w < g->ordem; w++)
-    {
-        if (g->adjacencias[v][w] == 1)
-        {
-            if (!g->visitados[w])
-            {
-                prof(w, g);
-            }
-        }
-    }
-}
-
-void profundidade(Grafo *g)
-{
-    int i;
-    limpa(g);
-
-    for (i = 0; i < g->ordem; i++)
-    {
-
-        if (!g->visitados[i])
-        {
-            prof(i, g);
-        }
-    }
-}
-
-int busca_profundidade_recursao(int origem, int destino, Grafo *g)
-{
-    if (origem == destino)
-    {
-        return 1;
-    }
-
-    g->visitados[origem] = 1;
-    printf(" PROFUNDIDADE %d \n", origem + 1);
-    for (int i = 0; i < g->ordem; i++)
-    {
-
-        if (g->adjacencias[origem][i] >= 1 && !g->visitados[i])
-        {
-            if (busca_profundidade_recursao(i, destino, g))
-            {
-                return 1;
-            }
-        }
-    }
-
-    return 0;
-}
-
-// retorna 1 se houver um caminho e 0 se nao houver
-int busca_profundidade(int origem, int destino, Grafo *g)
-{
-    limpa(g);
-    return busca_profundidade_recursao(origem, destino, g);
-}
-
-// retorna 1 se houver caminho e 0 se nao houver
-int busca_largura(int origem, int destino, Grafo *g)
-{
-    Fila *f = cria_fila();
-    int *w;
-    int i;
-
-    limpa(g);
-    g->visitados[origem] = 1;
-    w = (int *)malloc(sizeof(int));
-    *w = origem;
-    enqueue(f, (void *)w);
-
-    while (!eh_vazia_fila(f))
-    {
-        w = (int *)dequeue(f);
-        printf(" LARGURA %d \n", g->vertices[*w]);
-        if (*w == destino)
-        {
-            return 1;
-        }
-        for (i = 0; i < g->ordem; i++)
-        {
-            if (g->adjacencias[*w][i] == 1)
-            {
-                if (!g->visitados[i])
-                {
-                    g->visitados[i] = 1;
-                    w = (int *)malloc(sizeof(int));
-                    *w = i;
-                    enqueue(f, (void *)w);
-                }
-            }
-        }
-    }
-
-    return 0;
 }
 
 int verifica_grafo(Grafo *g)
@@ -467,23 +231,6 @@ int verifica_grafo(Grafo *g)
     }
     validacao = 1;
     return validacao;
-}
-
-void percurso_larg(Grafo *g, int vertice)
-{
-    int i = 0;
-    if (g->visitados[vertice - 1] != 1)
-    {
-        g->visitados[vertice - 1] = 1;
-        printf("%d ", vertice);
-        for (i = 0; i < g->ordem; i++)
-        {
-            if (g->adjacencias[vertice - 1][i] == 1)
-            {
-                percurso_larg(g, i + 1);
-            }
-        }
-    }
 }
 
 void sort_vertices_byValue(Listad *L)
@@ -521,7 +268,7 @@ void swapList(Nod *a, Nod *b)
     b->info = temp;
 }
 
-Listad *percurso_larg_lim(Grafo *g, int vertice, int lim, int vertice_inicial)
+void *percurso_larg_lim(Grafo *g, int vertice, int lim, int vertice_inicial)
 {
     int i = 0;
     if (g->visitados[vertice - 1] != 1)
